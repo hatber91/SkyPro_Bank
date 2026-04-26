166de1d652c4cf3c842841e031ef0b761c20dfb1
Hatber <hatber91@yandex.ru>
2026-04-26 22:12:57 +0500

    ver_3.2 - the debugged version of homework 10.1 - Продвинутый Git. Реализованы все задачи + ветка homework_10_1 + исправлены ошибки наставника.

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-from src import masks
-from src import widget
-from src import processing
+from src import masks, processing, widget
 
-#card_number = input('Введите код вашей карты, состоящий из 16 цифр на лицевой стороне пластиковой карты:')
-card_number = '1234567812345678'
+# card_number = input('Введите код вашей карты, состоящий из 16 цифр на лицевой стороне пластиковой карты:')
+card_number = "1234567812345678"
 print(masks.get_mask_card_number(card_number))
 
-#account_number = input('Введите номер вашего счёта, состоящий из 20 чисел: ')
-account_number = 12345678901234567890
+# account_number = input('Введите номер вашего счёта, состоящий из 20 чисел: ')
+account_number = "12345678901234567890"
 print(masks.get_mask_account(account_number))
 
-any_account_number = 'Visa Platinum 7000792289606361'
+any_account_number = "Visa Platinum 7000792289606361"
 
 print(widget.mask_account_card(any_account_number))
 
@@ -18,12 +16,14 @@ date_of_entry = "2024-03-11T02:26:18.671407"
 
 print(widget.get_date(date_of_entry))
 
-list_of_dictionaries = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+list_of_dictionaries = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 
-state_value = 'EXECUTED'
+state_value = "EXECUTED"
 
 print(processing.filter_by_state(list_of_dictionaries, state_value))
 
--- src/processing.py
+++ src/processing.py
@@ -13,7 +13,6 @@ def filter_by_state(list_of_dictionaries: Union[list, dict], state_value: Union[
     return filtered_list
 
 
-def sort_by_date(list_of_dictionaries: Union[list, dict], sorted_by_order=True) -> Union[list]:
+def sort_by_date(list_of_dictionaries: Union[list, dict], is_reverse: bool = True) -> Union[list]:
     """Функция сортирует список словарей по дате от большей к меньшей. Можно задать обратный порядок."""
-    return sorted(list_of_dictionaries, key=lambda item: item["date"], reverse=sorted_by_order)
-
+    return sorted(list_of_dictionaries, key=lambda item: item["date"], reverse=is_reverse)
