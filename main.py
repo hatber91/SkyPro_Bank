eb2b87ddecfb9b8d3b38d65d7f68210b4dcb2668
Hatber <hatber91@yandex.ru>
2026-04-26 19:09:13 +0500

    ver_3.0 - the debugged version of homework 10.1 - Продвинутый Git. Реализованы все задачи.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src import masks
 from src import widget
+from src import processing
 
 #card_number = input('Введите код вашей карты, состоящий из 16 цифр на лицевой стороне пластиковой карты: ')
 card_number = '1234567812345678'
@@ -16,3 +17,14 @@ print(widget.mask_account_card(any_account_number))
 date_of_entry = "2024-03-11T02:26:18.671407"
 
 print(widget.get_date(date_of_entry))
+
+list_of_dictionaries = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+state_value = 'EXECUTED'
+
+print(processing.filter_by_state(list_of_dictionaries, state_value))
+
+print(processing.sort_by_date(list_of_dictionaries, True))
---
+++ src/processing.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+
+def filter_by_state(list_of_dictionaries: Union[list, dict], state_value: Union[str]) -> Union[list]:
+    """Функция принимает список словарей и возвращает только словари по определённому ключу"""
+
+    filtered_list = []
+
+    for item in list_of_dictionaries:
+        if item.get("state") == state_value:
+            filtered_list.append(item)
+
+    return filtered_list
+
+
+def sort_by_date(list_of_dictionaries: Union[list, dict], sorted_by_order=True) -> Union[list]:
+    """Функция сортирует список словарей по дате от большей к меньшей. Можно задать обратный порядок"""
+    return sorted(list_of_dictionaries, key=lambda item: item["date"], reverse=sorted_by_order)
+
